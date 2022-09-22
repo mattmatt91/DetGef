b53fdec8cfec25386053a22962965ed9427d9a94
Matthias Muhr <matthias_muhr@web.de>
2022-09-22 15:45:37 +0200

    testing tcpclient

--- mfc_ether.py
+++ mfc_ether.py
@@ -1,22 +1,30 @@
+# total bytes 256  modbus message
+# 1 byte slave id, functioncode 1 byte, 0-252 bytes message, crc 2 bytes
+# functioncode is number from table 
+
+from pymodbus.client.sync import ModbusTcpClient
+
+from time import sleep
+
+host = '192.168.2.100'  #ip of your raspberry pi
+client = ModbusTcpClient(host)
+
+client.connect()
+
+wr = client.read_coils(0x06)
+print(wr)
+# sleep(1)
 
 
-import asyncio
-from mfc import FlowController
+# create client object
+client = ModbusSerial(host)
 
-ip = '192.168.2.100'
-async def get():
-    async with FlowController(ip) as fc:
-        print(await fc.get())
+# connect to device
+client.connect()
 
-async def set_gas(val):
-    async with FlowController(ip) as fc:
-        print(await fc.set_gas(val))
-        await fc.open()
-        
+# set/set information
+rr = client.read_coils(0x01)
+client.write_coil(0x01, 10)
 
-val  = 10
-asyncio.run(set_gas('N2'))
-asyncio.run(get())
-asyncio.run(set_gas('Air'))
-asyncio.run(get())
-
+# disconnect device
+client.close()
