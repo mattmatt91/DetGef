d13618e1fa57fc79cce031415584766b3bde43c3
mattmatt91 <matthias_muhr@web.de>
2023-03-01 13:18:20 +0100

    _

--- keysightdaq970A.py
+++ keysightdaq970A.py
@@ -1,63 +1,76 @@
 import pyvisa
 import time
-import pandas as pd
-from matplotlib import pyplot as plt
-
-# PW datasheet A1s2d3f4!address_keysightdaq970a = 'USB0::0x2A8D::0x5101::MY58018230::0::INSTR'# home
-address_keysightdaq970a = 'USB0::0x2A8D::0x5101::MY58018230::INSTR'  # rhb
 
 
 class KeysightDAQ970a():
-    def __init__(self, address):
-        self.i = 0
-        self.rm = pyvisa.ResourceManager()
-        self.client = self.rm.open_resource(address)
-        self.client.timeout = 2000  # set a delay
+    def __init__(self, address='TCPIP::K-DAQ970A-18230.local::5025::SOCKET'):
+        rm = pyvisa.ResourceManager()
+        print(rm.list_resources())
+
+        self.client = rm.open_resource(address)
+
+        self.aper_toggle = False
+        self.aperture = 0.06
+        self.nplc = 1
+        self.client.timeout = 10000  # set a delay
         self.client.read_termination = '\n'
-        self.scanlist = "(@201,202,203,204)"
+        self.scanlist = "(@101) # ,102,103,104)"
+        print(self.client.query("*IDN?"))
         self.client.write("*RST")
-        # print(self.client.query("*IDN?"))
+        self.client.write("*RST")
+        self.client.write("*RST")
+        self.client.write("*RST")
         self.client.write(":SYSTem:BEEPer:STATe 0")
+
+        # set resistance
+        self.client.write(f"SENSe:RESistance {self.scanlist}")
+
+        # set autorange
+        self.client.write("CONF:RES")
+        self.client.write("RES:RANG:AUTO 1")
+
+        # aperture or nplc
+        if self.aper:
+            # disables aperture
+            self.client.write(f"CONF:FRES")
+            self.client.write(f"FRES:NPLC {self.nplc}")
+        else:
+            self.client.write(
+                f":SENSe:RESistance:APERture:ENABle 1")
+            self.client.write(
+                f":SENSe:RESistance:APERture {self.aperture}")
 
     def get_errors(self):
         err = self.client.query("SYST:ERR?")
         return err
 
-    def set_param(self):
-        self.client.write(f":ROUTe:SCAN  {self.scanlist}")
-        self.client.write(
-            f":CONFigure:FRESistance AUTO,DEFault, {self.scanlist}")
-        self.client.write(f":SENSe:RESistance:RANGe:AUTO 1, {self.scanlist}")
-        self.client.write(
-            f":SENSe:RESistance:APERture:ENABle 1, {self.scanlist}")
-        self.client.write(f":SENSe:RESistance:APERture 0.02, {self.scanlist}")
-        self.client.write(f":SENSe:RESistance:NPLCycles 0.06, {self.scanlist}")
-
     def get_data(self):
         # self.client.write(":READ?")
         sensors = ['S1', 'S2', 'S3', 'S4']
-        try:
-            self.client.write(":INITiate")
-            data = self.client.query(":FETCh?")
-            data = [float(i) for i in data.split(',')]
-        except:
-            data = [None for _ in sensors]
+        # try:
+
+        data = self.client.query(f"READ?")
+        data = [float(i) for i in data.split(',')]
+        # except:
+        #     data = [None for _ in sensors]
         result = {}
         for sensor, value in zip(sensors, data):
-            result[sensor + "_resistance_multimeter_measured"] = value
+            result[sensor + "_res_measured"] = value
         return result
 
     def close(self):
         self.client.close()
-        self.rm.close()
 
 
 if __name__ == '__main__':
-    keysightdaq970a = KeysightDAQ970a(address_keysightdaq970a)
-    keysightdaq970a.set_param()
+    keysightdaq970a = KeysightDAQ970a()
     print(keysightdaq970a.get_errors())
-    for i in range(10):
-        print(keysightdaq970a.get_data())
-        print(keysightdaq970a.get_errors())
-        time.sleep(1)
+    print(keysightdaq970a.get_data())
+    print(keysightdaq970a.get_errors())
+    if 0:
+        for i in range(10):
+            print(keysightdaq970a.get_data())
+            print(keysightdaq970a.get_errors())
+            time.sleep(1)
+    print('exit')
     keysightdaq970a.close()
--- test.py
+++ test.py
@@ -1,85 +1,9 @@
-
-import visa
-'''
-Initialize the 34970A/72A and dirvers
-load visa lib and open connection
-Visa address used 'GPIB0::12::INSTR' can be found in Keysight Connection Expert
-'''
-rm = visa.ResourceManager()
-v34970A_2 = rm.open_resource('GPIB0::12::INSTR')    
-
-v34970A_2.timeout=5000      #set a delay
-v34970A_2.write("*CLS")     #clear 
-v34970A_2.write("*RST")     #reset 
-
-'''Set Variables'''
-scanIntervals = 10      #Delay in secs, between scans
-numberScans = 3         #Number of scan sweeps to measure
-channelDelay = 0.1      #Delay, in secs, between relay closure and measurement 
-points = 0              #number of data points stored
-voltage = 2.00          #voltage value to DAC from -12 to 12
-
-'''
-scanlist does not have to include all configured channels
-In this example 103 is excluded to illustrate
-'''
-scanlist = "(@101,102,110:112)"
-#setup channels configuration
-v34970A_2.write("CONF:TEMP TC,T,(@101)")  
-v34970A_2.write("CONF:TEMP TC,K,(@102)")  
-v34970A_2.write("CONF:TEMP THER,5000,(@103)")  
-v34970A_2.write("CONF:VOLT:DC (@110,111,112)")  
-#setup scan list
-v34970A_2.write("ROUTE:SCAN " + scanlist) 
-v34970A_2.write("ROUTE:SCAN:SIZE?") 
-numberChannels = int(v34970A_2.read())+1
-#reading format
-v34970A_2.write("FORMAT:READING:CHAN ON")
-v34970A_2.write("FORMAT:READING:TIME ON")  
-#channel delay
-v34970A_2.write("ROUT:CHAN:DELAY " + str(channelDelay)+","+scanlist)
-#setup when scanning starts and interval rate
-v34970A_2.write("TRIG:COUNT "+str(numberScans)) 
-v34970A_2.write("TRIG:SOUR TIMER")
-v34970A_2.write("TRIG:TIMER " + str(scanIntervals))
-#start the scan and retrieve the scan time
-v34970A_2.write("INIT;:SYSTEM:TIME:SCAN?")   
-print(v34970A_2.read())
-'''wait until there is a data available'''
-points = 0
-while (points==0):
-    v34970A_2.write("DATA:POINTS?")
-    points=int(v34970A_2.read())
-
-'''
-The data points are printed 
-data, time, channel
-'''
-for chan in range(1, numberChannels):
-    v34970A_2.write("DATA:REMOVE? 1")
-    print((v34970A_2.read()))
-    points = 0
-    #wait for data
-    while (points==0):
-        v34970A_2.write("DATA:POINTS?")
-        points=int(v34970A_2.read())
-     
-'''print(Inserted cards'''
-v34970A_2.write("SYST:CTYPE? 100") 
-print(v34970A_2.read()  )
-v34970A_2.write("SYST:CTYPE? 200") 
-print(v34970A_2.read() )
-v34970A_2.write("SYST:CTYPE? 300") 
-print(v34970A_2.read() )
-
-'''Write to DAC at port 200'''
-v34970A_2.write("SOURCE:VOLTAGE "+str(voltage)+" (@205)\n")     
-
-'''Close'''    
-v34970A_2.close()
-print('close instrument connection')
-    
-
+import pyvisa
+rm = pyvisa.ResourceManager()
+rm.list_resources()
+('ASRL1::INSTR', 'ASRL2::INSTR', 'GPIB0::12::INSTR')
+inst = rm.open_resource('GPIB0::12::INSTR')
+print(inst.query("*IDN?"))
 
 
 
